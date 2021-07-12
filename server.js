e1fadc869dc7e4935768f2e96aee1216a5681a1b
Julia Nash <julia@grrrl.tech>
2021-07-12 16:25:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
 require('dotenv').config();
 const cfenv = require("cfenv");
 const appEnv = cfenv.getAppEnv();
@@ -14,33 +13,6 @@ const dataRoutes = require('./routes/data');
 
 const app = express();
 
-
-// IBM Cloud DB for MongoDB setup -
-let ca = Buffer.from(process.env.CERTIFICATE_BASE64, "base64");
-
-let mongoDbOptions = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  ssl: true,
-  sslValidate: true,
-  useUnifiedTopology: true,
-  sslCA: ca
-};
-
-let mongoDB = process.env.MONGO_URL
-mongoose.connect(mongoDB, mongoDbOptions);
-
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-//- end of setup
-
-//check health
-db.once('open', function(){
-  console.log('connected to MongoDB 🦦');
-});
-
-// middlewarez
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
